2b3c05142fa5052f5c621c7f7f423c8d5ca1212c
shiyas03 <shiyastofficial@gmail.com>
2023-12-13 17:06:14 +0530

    side bar layout

--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+import { SideBarComponent } from './pages/side-bar/side-bar.component';
 
+import { NgIconsModule } from '@ng-icons/core';
+import { heroBars3, heroMagnifyingGlass, heroUserCircle, heroChevronDown, heroHome } from '@ng-icons/heroicons/outline';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgIconsModule.withIcons({ heroBars3, heroMagnifyingGlass, heroUserCircle, heroChevronDown,heroHome })
   ]
 })
 export class DashboardModule { }
