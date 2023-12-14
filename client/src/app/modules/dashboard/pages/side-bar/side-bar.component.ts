cf6e78f4626dadc36d96414a8a43cc96cc5b583a
shiyas03 <shiyastofficial@gmail.com>
2023-12-15 00:09:32 +0530

    sidebar dropdown align done

--- client/src/app/modules/dashboard/pages/side-bar/side-bar.component.ts
+++ client/src/app/modules/dashboard/pages/side-bar/side-bar.component.ts
@@ -1,15 +1,35 @@
-import { Component, Input } from '@angular/core';
-import { sideBarState } from './side-bar.animation';
+import { Component, ElementRef, HostListener, Input } from '@angular/core';
+import { sideBarState, dropDownMenuSate, buttonSlideState } from './side-bar.animation';
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.css'],
-  animations : [sideBarState]
+  animations: [sideBarState, dropDownMenuSate, buttonSlideState]
 })
 export class SideBarComponent {
 
   @Input() showFullBar: boolean = true
-  showToggle: boolean = false
-  
+  constructor(private _eref: ElementRef) { }
+
+  @HostListener('document:click', ['$event.target'])
+  onClick(event: HTMLElement) {
+    if (!(this._eref.nativeElement.contains(event))) {
+      this.homeDropDown = this.jobDropDown = false;
+    }
+  }
+
+  homeDropDown: boolean = false
+  jobDropDown: boolean = false
+
+  onHomeClick() {
+    this.homeDropDown = !this.homeDropDown
+    this.jobDropDown = false
+  }
+
+  onJobClick() {
+    this.jobDropDown = !this.jobDropDown
+    this.homeDropDown = false
+  }
+
 }
