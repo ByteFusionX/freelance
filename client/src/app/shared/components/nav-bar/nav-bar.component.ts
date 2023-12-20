99f88bb132f1d4663b2308ad840566e9740e42ac
shiyas03 <shiyastofficial@gmail.com>
2023-12-20 10:09:48 +0530

    navbar profile dropdown created

--- client/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ client/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { IconsModule } from 'src/app/lib/icons/icons.module';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
   standalone: true,
-  imports: [CommonModule, IconsModule]
+  imports: [CommonModule, IconsModule, MatMenuModule, MatButtonModule ]
 })
 export class NavBarComponent {
 
