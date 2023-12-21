8be9949bbf5e1291381aa8fdd449e58e6edce670
Diyouf <diyoufkv7@gmail.com>
2023-12-21 12:18:37 +0530

    Announcement date and dummy data added ,

--- client/src/app/modules/home/pages/announcements/announcements.component.ts
+++ client/src/app/modules/home/pages/announcements/announcements.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddAnnouncementComponent } from './add-announcement/add-announcement.component';
 
+
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
@@ -9,11 +10,21 @@ import { AddAnnouncementComponent } from './add-announcement/add-announcement.co
 })
 export class AnnouncementsComponent {
   constructor(public dialog: MatDialog) { }
+
   openDialog() {
     const dialogRef = this.dialog.open(AddAnnouncementComponent);
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+      // console.log(`Dialog result: ${result}`);
     });
   }
+
+  announcementData: any = [
+    { title: 'Onam Holidays', description: 'In this example, the removeTimeFromDate function sets the hours, minutes, seconds, and milliseconds to zero, effectively removing the time component from the date. Then, you can use the getTime() method to get the timestamp for each date and compare them. If the timestamps are equal, the dates are considered equal', date: '12-12-2023', postedDate: new Date() },
+    { title: 'Christmas Holidays', description: 'In this example, the removeTimeFromDate function sets the hours, minutes, seconds, and milliseconds to zero, effectively removing the time component from the date. Then, you can use the getTime() method to get the timestamp for each date and compare them. If the timestamps are equal, the dates are considered equal', date: '12-12-2023', postedDate: new Date() },
+    { title: 'Holi Holidays', description: 'In this example, the removeTimeFromDate function sets the hours, minutes, seconds, and milliseconds to zero, effectively removing the time component from the date. Then, you can use the getTime() method to get the timestamp for each date and compare them. If the timestamps are equal, the dates are considered equal', date: '12-12-2023', postedDate: new Date() },
+  ]
+  recentData = this.announcementData.shift()
+
+
 }
