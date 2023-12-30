9f5a1a1dd96a0f22bc3faf22a8d0ca57132276fa
Basim Mohammed <107759928+Basimohd@users.noreply.github.com>
2023-12-30 12:09:59 +0530

    Department model created

---
+++ server/src/models/department.model.ts
@@ -0,0 +1,26 @@
+import { Schema, Document, model, Types } from "mongoose";
+
+interface Department extends Document {
+    departmentName: string;
+    departmentHead: Types.ObjectId;
+    createdDate: Date;
+}
+
+const departmentSchema = new Schema<Department>({
+    departmentName: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    departmentHead: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default model<Department>("Department", departmentSchema);
