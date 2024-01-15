e576a3732ca8cd5375ba9640a12c41d3e2d2b68e
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2024-01-15 11:49:39 +0530

    profile completed

--- server/src/routes/employee.router.ts
+++ server/src/routes/employee.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createEmployee, getEmployee,login } from "../controllers/employee.controller";
+import { createEmployee, getEmployee,getEmployeeData,login } from "../controllers/employee.controller";
 const empRouter = Router()
 
 empRouter.get('/',getEmployee)
 empRouter.post('/',createEmployee)
 empRouter.post('/login',login)
+empRouter.get('/getEmployeeData/:employeeId',getEmployeeData)
 
 export default empRouter;
