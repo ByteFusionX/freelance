b3d7bccb67523d6f9cb216d2c4408fe42ffa80d1
shiyas03 <shiyastofficial@gmail.com>
2024-01-23 21:13:35 +0530

    dashboard user name displayed

--- client/src/app/modules/home/pages/dashboard/dashboard.component.ts
+++ client/src/app/modules/home/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ChartOptions } from './dashboard.chart';
 import { EnquiryService } from 'src/app/core/services/enquiry/enquiry.service';
 import { Observable, Subscription } from 'rxjs';
 import { TotalEnquiry } from 'src/app/shared/interfaces/enquiry.interface';
+import { EmployeeService } from 'src/app/core/services/employee/employee.service';
+import { getEmployee } from 'src/app/shared/interfaces/employee.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,7 +12,7 @@ import { TotalEnquiry } from 'src/app/shared/interfaces/enquiry.interface';
   styleUrls: ['./dashboard.component.css']
 })
 
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
 
   enquiries$!: Observable<TotalEnquiry[]>;
   quotes!: number;
@@ -18,36 +20,24 @@ export class DashboardComponent implements OnInit {
   graphSeries: { name: string, data: number[] }[] = []
   graphCategory: string[] = []
   isLoading: boolean = true
+  userData$!: Observable<getEmployee>;
 
   private subscriptions = new Subscription()
   public chartOptions!: Partial<ChartOptions>;
 
-  constructor(private enquiryService: EnquiryService) { }
+  constructor(private enquiryService: EnquiryService, private _employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    let token = this._employeeService.employeeToken()
+    this.userData$ = this._employeeService.getEmployee(token.id)
     this.enquiries$ = this.enquiryService.totalEnquiries()
     this.loading()
     this.dateCategories()
-    this.subscriptions.add(
-      this.enquiryService.monthlyEnquiries().subscribe((data) => {
-        data.map((item) => {
-          const dateArray: number[] = new Array(12).fill(0)
-          let depName = item.department[0].departmentName.toUpperCase()
-          let dep = this.graphSeries.find((ser) => ser.name == depName)
+    this.getChartDetails()
+  }
 
-          if (!dep) {
-            let obj = { name: depName, data: dateArray }
-            this.graphSeries.push(obj)
-            dep = this.graphSeries[this.graphSeries.length - 1]
-          }
-
-          let date: string = `${item.year}-${item.month.toString().padStart(2, '0')}`
-          let index = this.graphCategory.indexOf(date)
-          dep.data[index] = item.total
-        })
-        this.chartDetails()
-      })
-    )
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe()
   }
 
   loading() {
@@ -69,6 +59,29 @@ export class DashboardComponent implements OnInit {
       this.graphCategory.unshift(formattedDate);
       currentDate.setMonth(currentDate.getMonth() - 1);
     }
+  }
+
+  getChartDetails() {
+    this.subscriptions.add(
+      this.enquiryService.monthlyEnquiries().subscribe((data) => {
+        data.map((item) => {
+          const dateArray: number[] = new Array(12).fill(0)
+          let depName = item.department[0].departmentName.toUpperCase()
+          let dep = this.graphSeries.find((ser) => ser.name == depName)
+
+          if (!dep) {
+            let obj = { name: depName, data: dateArray }
+            this.graphSeries.push(obj)
+            dep = this.graphSeries[this.graphSeries.length - 1]
+          }
+
+          let date: string = `${item.year}-${item.month.toString().padStart(2, '0')}`
+          let index = this.graphCategory.indexOf(date)
+          dep.data[index] = item.total
+        })
+        this.chartDetails()
+      })
+    )
   }
 
   chartDetails() {
