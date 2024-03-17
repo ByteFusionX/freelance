3ae851087c929db4d7a2d427233266c1e062ef62
Diyouf <diyoufkv7@gmail.com>
2024-03-17 15:20:35 +0530

    CelebCheck(Update):Api limit included

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { celebCheckService } from './core/services/celebrationCheck/celebCheck.service';
 import { announcementGetData } from './shared/interfaces/announcement.interface';
-import { concatMap, from, interval, take, switchMap, takeUntil } from 'rxjs';
+import { concatMap, from, interval, take, switchMap, takeUntil, Subscription } from 'rxjs';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CelebrationDialogComponent } from './shared/components/celebration-dialog/celebration-dialog.component';
 import { Subject } from 'rxjs';
@@ -12,73 +13,96 @@ import { Subject } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnDestroy, OnInit {
   title = 'client';
   birthdaysViewed!: boolean;
-  reduceSate: boolean = true;
-  dialogRef: MatDialogRef<CelebrationDialogComponent, any> | undefined;
+  reduceState: boolean = true;
+  loginRouter: boolean = false;
+  dialogRef: MatDialogRef<CelebrationDialogComponent> | undefined;
+  employeeToken: string | null = null;
   private destroy$ = new Subject<void>();
+  private subscriptions: Subscription = new Subscription()
 
-  constructor(private route: ActivatedRoute, private _service: celebCheckService, private dialog: MatDialog) { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private _service: celebCheckService,
+    private dialog: MatDialog,
+    private router: Router
+  ) { }
 
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+    this.subscriptions.unsubscribe()
   }
 
   ngOnInit() {
-    this.getCelebData();
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.loginRouter = this.isLoginRoute();
+        if (!this.loginRouter) {
+          this.isUserThere();
+        }
+      }
+    });
   }
 
   reduceSideBar(event: boolean) {
-    this.reduceSate = event;
+    this.reduceState = event;
   }
 
   isLoginRoute(): boolean {
     return this.route.snapshot.firstChild?.routeConfig?.path === 'login';
   }
 
+  isUserThere() {
+    this.employeeToken = localStorage.getItem('employeeToken');
+    this.getCelebData();
+  }
 
   getCelebData() {
-    this.birthdaysViewed = this._service.hasTodaysBirthdaysBeenViewed();
-    if (!this.birthdaysViewed) {
-      this._service.getCelebrationData().subscribe((data) => {
-        if (data && data.length > 0) {
-          from(data).pipe(
-            concatMap((item, index) => {
-              return interval(1000 * index).pipe(
-                take(1),
-                switchMap(() => {
-                  if (this.dialogRef && this.dialogRef.componentInstance) {
-                    return this.dialogRef.afterClosed().pipe(
-                      takeUntil(this.destroy$),
-                      switchMap(() => {
+    if (this.employeeToken) {
+      this.birthdaysViewed = this._service.hasTodaysBirthdaysBeenViewed();
+      if (!this.birthdaysViewed) {
+        this.subscriptions.add(
+          this._service.getCelebrationData().subscribe((data) => {
+            if (data && data.length > 0) {
+              from(data).pipe(
+                concatMap((item, index) => {
+                  return interval(1000 * index).pipe(
+                    take(1),
+                    switchMap(() => {
+                      if (this.dialogRef && this.dialogRef.componentInstance) {
+                        return this.dialogRef.afterClosed().pipe(
+                          takeUntil(this.destroy$),
+                          switchMap(() => {
+                            this.dialogRef = this.openCelebrationDialog(item);
+                            return [];
+                          })
+                        );
+                      } else {
                         this.dialogRef = this.openCelebrationDialog(item);
                         return [];
-                      })
-                    );
-                  } else {
-                    this.dialogRef = this.openCelebrationDialog(item);
-                    return [];
-                  }
-                })
-              );
-            }),
-            takeUntil(this.destroy$)
-          ).subscribe();
-          this._service.markTodaysBirthdaysAsViewed();
-        }
-      });
+                      }
+                    })
+                  );
+                }),
+                takeUntil(this.destroy$)
+              ).subscribe();
+              
+            }
+          })
+        )
+      }
+      this._service.markTodaysBirthdaysAsViewed();
     }
   }
-  
 
-  openCelebrationDialog(data: announcementGetData): MatDialogRef<CelebrationDialogComponent, any> {
+  openCelebrationDialog(data: announcementGetData): MatDialogRef<CelebrationDialogComponent> {
     return this.dialog.open(CelebrationDialogComponent, {
       data: data,
       width: '400px',
     });
   }
-
-  
 }
