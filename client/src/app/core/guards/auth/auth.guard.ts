89aafdab3cef28ab0cdbd7c5f34acf98ac38edf0
Basim Mohammed <107759928+Basimohd@users.noreply.github.com>
2024-04-06 08:38:51 +0530

    login guard changed

--- client/src/app/core/guards/auth/auth.guard.ts
+++ client/src/app/core/guards/auth/auth.guard.ts
@@ -5,16 +5,9 @@ import { Location } from '@angular/common';
 export const AuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   const token = <string>localStorage.getItem('employeeToken')
   const router: Router = inject(Router);
-  const location = inject(Location)
-  let isLogin: boolean = state.url.includes('login')
-
-  if (isLogin === true && !token || isLogin === false && token) {
-    return true
-  } else if (!isLogin && !token) {
+  if (!token) {
     router.navigate(['/login'])
     return false
-  } else {
-    location.back()
-    return false
-  }
+  } 
+  return true;
 };
